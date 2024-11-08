11688b1671cd65ad5f4e549207c247046da1ffef
LarsMadproduction <schumacher90lars@gmail.com>
2024-11-08 14:58:47 +0100

    bugs fixed

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -4,18 +4,27 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-player',
   standalone: true,
-  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule],
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+  ],
   templateUrl: './dialog-add-player.component.html',
   styleUrl: './dialog-add-player.component.scss',
 })
 export class DialogAddPlayerComponent {
+  constructor(private dialogRef: MatDialogRef<DialogAddPlayerComponent>) {}
+
   name: string = '';
 
-  onNoClick(){
-    
+  onNoClick() {
+    this.dialogRef.close();
   }
 }
