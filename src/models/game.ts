b31d27329a0725ed44dbb877588d5191f5f119fb
LarsMadproduction <schumacher90lars@gmail.com>
2024-11-08 13:19:52 +0100

    added player variable and design

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-  public players: string[] = ['Lars', 'Natascha', 'Johanna'];
+  public players: string[] = [];
   public stack: string[] = [];
   public playedCards: string[] = [];
   public currentPlayer: number = 0;
